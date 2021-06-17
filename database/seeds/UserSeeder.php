0ab7fe71dca4996149600e6a3e1c49dc0032f1ee
michele <micheleepedrotti@gmail.com>
2021-06-18 01:34:12 +0200

    DONE: fixed dummy user password crypt
    - moved dummy user in all users seeder
    - removed DummyUsersSeeder
    - fixed password bug with bcrypt()

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -12,6 +12,16 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
+        $dummyUser = [
+            'company_name' => 'company', 
+            'email' => 'company@mail.com',
+            'password' => bcrypt('password'),
+            'address' => 'via di qua, 1',
+            'vat_number' => '99999999999',
+        ];
+
+        User::create($dummyUser);
+
         factory(User::class, 10) -> create();
     }
 }
