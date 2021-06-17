f6fc8a67912e7d53c2dc63795770a3f86b2164ab
Marco Orazio Re <marcooraziore@gmail.com>
2021-06-17 16:01:46 +0200

    DONE: Factories
    - All needed factories populated

--- database/factories/DishFactory.php
+++ database/factories/DishFactory.php
@@ -7,6 +7,11 @@ use Faker\Generator as Faker;
 
 $factory->define(Dish::class, function (Faker $faker) {
     return [
-        //
+        'name'        => $faker -> word(),
+        'ingredients' => $faker -> word(),
+        'discount'    => $faker -> numberBetween(10, 50),
+        'price'       => $faker -> randomFloat($nbMaxDecimals = 2, $min = 2, $max = 15),
+        'img'         => $faker -> imageUrl($width = 640, $height = 480),
+        'type'        => $faker -> word()
     ];
 });
--- database/factories/RestaurantFactory.php
+++ database/factories/RestaurantFactory.php
@@ -7,6 +7,14 @@ use Faker\Generator as Faker;
 
 $factory->define(Restaurant::class, function (Faker $faker) {
     return [
-        //
+        'name'          => $faker -> word(),
+        'address'       => $faker -> address(),
+        'email'         => $faker -> email(),
+        'telephone'     => $faker -> companyNumber,
+        'description'   => $faker -> text($maxNbChars = 300),
+        'img_cover'     => $faker -> imageUrl($width = 640, $height = 480),
+        'logo'          => $faker -> imageUrl($width = 300, $height = 300),
+        'allow_cash'    => $faker -> boolean($chanceOfGettingTrue = 50),
+        'delivery_cost' => $faker -> randomFloat(0, 20),
     ];
 });
