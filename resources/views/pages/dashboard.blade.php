3167647386c4603dda427e18c985acec72b977c7
michele <micheleepedrotti@gmail.com>
2021-06-18 02:34:42 +0200

    Done: created an initial structure for blades and scss files
    -  managed routes for initial page and login redirect
    - created views/scss new folders and file for handle layouts and components
    - 3 layouts -> home, main, dashboard
    - changed some style in scss to differenciate layouts

---
+++ resources/views/pages/dashboard.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.dashboard-layout')
+
+@section('content')
+
+    <div id="dashboard-title">
+        <h1>
+            Questa è la dashboard del nostro sito.
+        </h1>
+    </div>
+
+@endsection
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.home-layout')
+
+@section('content')
+
+    <div id="home-title">
+        <h1 class="text-center">
+            Questa è la home del nostro sito
+        </h1>
+    </div>
+
+@endsection
