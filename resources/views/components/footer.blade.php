1855c7243705b2771b8981ab89d86175809f4962
Alberto Gambino <gambafb@libero.it>
2021-06-18 11:15:41 +0100

    WIP: link immagini

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,182 @@
+<footer>
+
+        <div class="footer-bg">
+
+            <div class="footer-card-container">
+
+                <div class="footer-card">
+
+                    <ul>
+
+                        <li class="footer-bolder">
+                            <a href="">Scopri Deliveroo</a>
+                        </li>
+
+                        <li>
+                            <a href="">Investitori</a>
+                        </li>
+
+                        <li>
+                            <a href="">Chi siamo</a>
+                        </li>
+
+                        <li>
+                            <a href="">Ristoranti</a>
+                        </li>
+
+                        <li>
+                            <a href="">Altro</a>
+                        </li>
+
+                        <li>
+                            <a href="">Pressroom</a>
+                        </li>
+
+                        <li>
+                            <a href="">Il nostro blog</a>
+                        </li>
+
+                        <li>
+                            <a href="">Programmazione</a>
+                        </li>
+
+                        <li>
+                            <a href="">Design</a>
+                        </li>
+
+                        <li>
+                            <a href="">Lavora con noi</a>
+                        </li>
+
+                        <li>
+                            <a href="">Diventa nostro partner</a>
+                        </li>
+
+                    </ul>
+
+
+
+                </div>
+                <div class="footer-card second-card-footer">
+                    <ul>
+
+                        <li class="footer-bolder">
+                            <a href="">Note legali</a>
+                        </li>
+
+                        <li>
+                            <a href="">Termini & Condizioni</a>
+                        </li>
+
+                        <li>
+                            <a href="">Informativa sulla privacy</a>
+                        </li>
+
+                        <li>
+                            <a href="">Cookies</a>
+                        </li>
+
+                    </ul>
+
+
+
+                </div>
+                <div class="footer-card second-card-footer">
+                    <ul>
+
+                        <li class="footer-bolder">
+                            <a href="">Aiuto</a>
+                        </li>
+
+                        <li>
+                            <a href="">Contatti</a>
+                        </li>
+
+                        <li>
+                            <a href="">FAQ</a>
+                        </li>
+
+                        <li>
+                            <a href="">Tipi di cucina</a>
+                        </li>
+
+                        <li>
+                            <a href="">Mappa del sito</a>
+                        </li>
+
+                    </ul>
+
+
+
+                </div>
+                <div class="footer-card second-card-footer">
+                    <ul>
+
+                        <li class="footer-bolder">
+                            <a href="">Porta Deliveroo con te</a>
+                        </li>
+
+                        <li>
+                            <a href="">
+                                <img src="images/AppStore.png" alt="">
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="">
+                                <img src="{{asset('/storage/images/Googleplay.png')}}" alt="">
+                            </a>
+                        </li>
+
+                    </ul>
+
+
+
+                </div>
+
+
+
+
+            </div>
+
+            <div class="icons-footer">
+
+                <div class>
+
+                   <a href="">
+
+                        <span><i class="fab fa-facebook"></i></span>
+
+                   </a>
+                    <a href="">
+
+                        <span><i class="fab fa-twitter"></i></span>
+
+                    </a>
+                    <a href="">
+
+                        <span><i class="fab fa-instagram"></i></span>
+
+                    </a>
+
+
+
+
+
+                </div>
+                <div>
+
+                </div>
+
+                <div>
+
+                    <span class="copyright-footer">© 2021 Deliveboo</span>
+
+                </div>
+
+            </div>
+
+
+        </div>
+
+    </footer>
