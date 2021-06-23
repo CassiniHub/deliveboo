9d0057b79beb9a2dc7b44cfa4be4f1ba7b2bb3c3
Marco Orazio Re <marcooraziore@gmail.com>
2021-06-23 10:53:27 +0200

    DONE: Dish create / store
    - Create / Store CRUD created

--- app/Http/Controllers/DishController.php
+++ app/Http/Controllers/DishController.php
@@ -3,10 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Dish;
+use App\Restaurant;
 
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Auth;
 Use App\Library\Helpers\MyValidation;
+
+Use App\Library\Helpers\Images;
+use Illuminate\Support\Facades\Storage;
+use File;
 
 class DishController extends Controller
 {
@@ -31,7 +36,7 @@ class DishController extends Controller
      */
     public function create()
     {
-        //
+        return view('pages.dishes.create');
     }
 
     /**
@@ -42,7 +47,25 @@ class DishController extends Controller
      */
     public function store(Request $request)
     {
-        //
+
+        $validateData = $request -> validate(MyValidation::validateDish());
+        $dish         = Dish::make($validateData);
+
+        $restaurant = Restaurant::findOrFail($request -> get('restaurant_id'));
+
+        if ($request -> file('img')) {
+            $image      = new Images;
+            $imgNewName = $image -> getImgName($request, 'img');
+            $folderPath = '/images/dishes';
+            $storedImg  = ($request -> file('img'))
+                -> storeAs($folderPath, $imgNewName, 'public');
+            $dish -> img = $imgNewName;
+        }
+
+        $dish -> restaurant() -> associate($restaurant -> id);
+        $dish -> save();
+
+        return redirect() -> route('protectedRestaurant.show', $restaurant -> id);
     }
 
     /**
