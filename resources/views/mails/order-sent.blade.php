6c569de973ed523fbed95681f74d9395142c0498
michele <micheleepedrotti@gmail.com>
2021-07-01 09:37:46 +0200

    DONE: send mail when order is ready

---
+++ resources/views/mails/order-sent.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=ntent="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+    <h2>
+        Il rider è appena partito dal ristorante!
+    </h2>
+    <p>
+        Grazie per aver scelto Deliveboo.
+    </p>
+
+</body>
+</html>
