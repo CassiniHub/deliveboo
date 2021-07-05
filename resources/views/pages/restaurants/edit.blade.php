ddfd383ba22f47ded00a04163860129f2c62f03f
michele <micheleepedrotti@gmail.com>
2021-07-05 15:51:30 +0200

    DONE: changed all forms labels to ita

--- resources/views/pages/restaurants/edit.blade.php
+++ resources/views/pages/restaurants/edit.blade.php
@@ -22,7 +22,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">New Restaurant</div>
+                <div class="card-header">{{ $restaurant ->name }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('restaurants.update', $restaurant) }}" enctype="multipart/form-data">
@@ -30,7 +30,7 @@
                         @csrf
 
                         <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">Name</label>
+                            <label for="name" class="col-md-4 col-form-label text-md-right">Nome</label>
 
                             <div class="col-md-6">
                                 <input id="name" type="text" class="form-control"  name="name" minlength="2" maxlength="255" value="{{ $restaurant ->name }}" required autofocus>
@@ -38,7 +38,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="address" class="col-md-4 col-form-label text-md-right">Address</label>
+                            <label for="address" class="col-md-4 col-form-label text-md-right">Indirizzo</label>
 
                             <div class="col-md-6">
                                 <input id="address" type="text" class="form-control"  name="address" minlength="6" maxlength="255" value="{{ $restaurant ->address }}" required>
@@ -46,7 +46,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">E-mail</label>
+                            <label for="email" class="col-md-4 col-form-label text-md-right">Indirizzo e-mail</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="text" class="form-control"  name="email" maxlength="128" value="{{ $restaurant ->email }}" required>
@@ -54,7 +54,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="telephone" class="col-md-4 col-form-label text-md-right">Phone number</label>
+                            <label for="telephone" class="col-md-4 col-form-label text-md-right">Numero di telefono</label>
 
                             <div class="col-md-6">
                                 <input id="telephone" type="text" class="form-control"  name="telephone" maxlength="32" value="{{ $restaurant ->telephone }}" required>
@@ -63,7 +63,7 @@
 
 
                         <div class="form-group row">
-                            <label for="img_cover" class="col-md-4 col-form-label text-md-right">Cover image</label>
+                            <label for="img_cover" class="col-md-4 col-form-label text-md-right">Immagine di copertina</label>
 
                             <div class="col-md-6">
                                 <input id="img_cover" type="file" class="form-control-file"  name="img_cover" >
@@ -71,7 +71,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="logo" class="col-md-4 col-form-label text-md-right">Restaurant logo</label>
+                            <label for="logo" class="col-md-4 col-form-label text-md-right">Llogo</label>
 
                             <div class="col-md-6">
                                 <input id="logo" type="file" class="form-control-file"  name="logo">
@@ -79,7 +79,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="form-check" class="col-md-4 col-form-label text-md-right">Allow cash</label>
+                            <label for="form-check" class="col-md-4 col-form-label text-md-right">Pagamento in contanti</label>
 
                             <div class="form-check my-2 mx-5">
                                 <input class="form-check-input" type="radio" name="allow_cash" id="allow_cash" value="1"
@@ -102,7 +102,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="delivery_cost" class="col-md-4 col-form-label text-md-right">Delivery cost</label>
+                            <label for="delivery_cost" class="col-md-4 col-form-label text-md-right">Costo aggiuntivo consegna</label>
 
                             <div class="col-md-6">
                                 <input id="delivery_cost" type="number" value='{{ $restaurant ->delivery_cost }}' step=".01" min="0" class="form-control"  name="delivery_cost"  required>
@@ -110,7 +110,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="description" class="col-md-4 col-form-label text-md-right">Description</label>
+                            <label for="description" class="col-md-4 col-form-label text-md-right">Descrizione</label>
 
                             <div class="col-md-6">
                                 <textarea name="description" id="description" cols="35" rows="5">
@@ -140,7 +140,7 @@
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    Edit
+                                    Salva le modifiche
                                 </button>
                             </div>
                         </div>
